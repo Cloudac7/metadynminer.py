83de35bfc91c81c185895deeb0537c79074c553a
Cloudac7 <812556867@qq.com>
2023-06-14 18:12:54 +0800

    fix(typo): remove wrong unittest

--- tests/test_fes.py
+++ tests/test_fes.py
@@ -17,7 +17,7 @@ def plumed_hills(filename, cvs, resolution=256):
     "name, periodic, cv_per, resolution", [
         ("acealanme1d", [True], [[-np.pi, np.pi]], 256),
         ("acealanme", [True,True], [[-np.pi, np.pi], [-np.pi, np.pi]], 256),
-        ("acealanme3d", [True,True,True], [[-np.pi, np.pi], [-np.pi, np.pi], [-np.pi, np.pi]], 64)
+        # ("acealanme3d", [True,True,True], [[-np.pi, np.pi], [-np.pi, np.pi], [-np.pi, np.pi]], 64)
     ]
 )
 def test_makefes(shared_datadir, name, periodic, cv_per, resolution):
